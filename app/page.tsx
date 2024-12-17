0910ea6d5c1ab2820c10bae03acde41f567acc0e
Rafael <raf.gomes.reis@gmail.com>
2024-12-16 22:15:54 -0300

    fazendo o loading certinho

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,45 @@
-"use client"
+"use client";
 
 import { User } from "@/types/User";
 import { useEffect, useState } from "react";
 import { serialize } from "v8";
 
-const Page = () =>{
-
+const Page = () => {
+  const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState<User[]>([]);
 
-  useEffect(() =>{
-
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(json => {
-      setUsers(json);
-    })
-
-
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setLoading(false);
+        setUsers(json);
+      })
+      .catch(() => {
+        console.log("error 404");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
 
-    return (
-      <div className="container mx-auto">
-        <h1 className="text-3xl ">Lista de usuários</h1>
-
-      {users.length <= 0 && "carregando......."}
-      {users.length > 0 && 
-
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl ">Lista de usuários</h1>
+      {loading && "loading.."}
+      {!loading && users.length > 0 && (
         <ul>
-          {users.map(item => (
-            <li key={item.id}>{item.name} ({item.address.city})</li>
+          {users.map((item) => (
+            <li key={item.id}>
+              {item.name} ({item.address.city})
+            </li>
           ))}
         </ul>
-      }
-      </div>
-    );
+      )}
+      {!loading && users.length === 0 && "não há usuarios para exibir"}
+    </div>
+  );
 };
 
 export default Page;
