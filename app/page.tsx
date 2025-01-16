198f1efb69ec49222426c16e92846d362b0a5f1c
Rafael <raf.gomes.reis@gmail.com>
2025-01-15 23:07:55 -0300

    enviando arquivos via fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,42 @@
 "use client";
 
 import { User } from "@/types/User";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { serialize } from "v8";
 
 const Page = () => {
-  const [loading, setLoading] = useState(false);
-  const [users, setUsers] = useState<User[]>([]);
+  const [legendInput, setLegendInput] = useState("");
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const getUsers = async() => {
-    try{
-    setLoading(true);
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const json = await res.json();
-    setUsers(json);
-  
-    } catch (err) {
-      console.log("failed totall");
+  const HandleFileSend = async () => {
+    if(fileInputRef.current?.files && fileInputRef.current.files.length > 0){
+      const fileItem = fileInputRef.current.files[0];
+
+      console.log(fileItem)
+
+
+    } else {
+      alert("selecione um arquivo")
     }
-    setLoading(false);
   }
 
-
-  useEffect(() => {
-    getUsers();
-    // setLoading(true);
-    // fetch("https://jsonplaceholder.typicode.com/users")
-    //   .then((res) => res.json())
-    //   .then((json) => {
-    //     setLoading(false);
-    //     setUsers(json);
-    //   })
-    //   .catch(() => {
-    //     console.log("error 404");
-    //   })
-    //   .finally(() => {
-    //     setLoading(false);
-    //   });
-  }, []);
-
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl ">Lista de usuários</h1>
-      {loading && "loading.."}
-      {!loading && users.length > 0 && (
-        <ul>
-          {users.map((item) => (
-            <li key={item.id}>
-              {item.name} ({item.address.city})
-            </li>
-          ))}
-        </ul>
-      )}
-      {!loading && users.length === 0 && "não há usuarios para exibir"}
+    <div className="container mx-auto ">
+      <h1 className="text 3xl mt-4">upload de imagem</h1>
+      <div className="flex flex-col gap-3 max-w-md border border-dotted border-white p-3 mt-4">
+        <input 
+          type="file" 
+          ref={fileInputRef} 
+        />
+        <input
+          type="text"
+          placeholder="Digite uma legenda"
+          className="p-3 bg-white rounded-md text-black"
+          value={legendInput}
+          onChange={e => setLegendInput(e.target.value)}
+        />
+        <button onClick={HandleFileSend}>Enviar imagem</button>
+      </div>
     </div>
   );
 };
